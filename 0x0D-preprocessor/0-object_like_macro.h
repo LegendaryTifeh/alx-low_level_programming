ab332f4642105b23fe7d177999b8b732b7e74cbc
LegendaryTifeh <legendarytife@gmail.com>
2023-08-14 14:15:48 -0700

    cr7

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif /* OBJECT_LIKE_MACRO_H */
+
+task 1
+#ifndef PI_H
+#define PI_H
+
+#define PI 3.14159265359
+
+#endif /* PI_H */
